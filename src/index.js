4d37c8eb15563ae93b4b5407efb1677e44850c9d
Huyenvtn <huyenvtn0210@gmail.com>
2021-07-22 15:35:01 +0700

    Replace class with function - TodoList and todoItem

--- src/index.js
+++ src/index.js
@@ -30,50 +30,75 @@ class ToDoForm extends React.Component {
       )
   }
 }
-class ToDoItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onClickClose = this.onClickClose.bind(this);
-    this.onClickDone = this.onClickDone.bind(this);
-  }
-  onClickClose() {
-    var index = parseInt(this.props.index);
-    this.props.removeItem(index);
-  }
-  onClickDone() {
-    var index = parseInt(this.props.index);
-    this.props.doneItem(index);
-  }
-  render() {
-    var todoClass = this.props.item.done ? "done" : "undone";
-    return (
-      <li className="list-group-item">
-        <div className={todoClass}>
-          <span className="glyphicon glyphicon-ok icon" aria-hidden="true" onClick={this.onClickDone}></span>
-              {this.props.item.value}
-          <button className="close" onClick={this.onClickClose}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
-          </button>
-        </div>
-      </li>
-      )
-  }
-}
-class ToDoList extends React.Component {
 
-  render() {
-    var tasks = this.props.items.map((item, index)=> {
-      return (
-        <ToDoItem 
-        key={index} index={index} item={item} removeItem={this.props.removeItem} doneItem={this.props.doneItem }/>
-      );
-    });
-    return (     
-      <ul className="list-group">  
-        {tasks}
-      </ul>
-    );  
-  }
+const ToDoItem = (props) => {
+  var todoClass = props.item.done ? "done" : "undone";
+  return (
+    <li className="list-group-item">
+      <div className={todoClass}>
+          <span className="glyphicon glyphicon-ok icon" aria-hidden="true" onClick={()=>{ props.doneItem(parseInt(props.index)) }}></span>
+          {props.item.value} 
+          <span className="glyphicon glyphicon-remove close" aria-hidden="true" onClick={()=>{ props.removeItem(parseInt(props.index))}}></span>
+      </div>
+    </li>
+  );
 }
+
+// class ToDoItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.onClickClose = this.onClickClose.bind(this);
+//     this.onClickDone = this.onClickDone.bind(this);
+//   }
+//   onClickClose() {
+//     var index = parseInt(this.props.index);
+//     this.props.removeItem(index);
+//   }
+//   onClickDone() {
+//     var index = parseInt(this.props.index);
+//     this.props.doneItem(index);
+//   }
+//   render() {
+//     var todoClass = this.props.item.done ? "done" : "undone";
+//     return (
+//       <li className="list-group-item">
+//         <div className={todoClass}>
+//           <span className="glyphicon glyphicon-ok icon" aria-hidden="true" onClick={this.onClickDone}></span>
+//               {this.props.item.value}
+//           <button className="close" onClick={this.onClickClose}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
+//           </button>
+//         </div>
+//       </li>
+//       )
+//   }
+// }
+
+const ToDoList = (props) => {
+  var tasks = props.items.map((item, index)=> {
+    return (
+      <ToDoItem key={index} index={index} item={item} removeItem={props.removeItem} doneItem={props.doneItem}/>
+    );
+  });
+  return (
+    <ul className="list-group">{tasks}</ul>
+  );
+}
+
+// class ToDoList extends React.Component {
+//   render() {
+//     var tasks = this.props.items.map((item, index)=> {
+//       return (
+//         <ToDoItem 
+//         key={index} index={index} item={item} removeItem={this.props.removeItem} doneItem={this.props.doneItem }/>
+//       );
+//     });
+//     return (     
+//       <ul className="list-group">  
+//         {tasks}
+//       </ul>
+//     );  
+//   }
+// }
 
 function ToDoHeader () {
     return (<h1>To Do List</h1>);
